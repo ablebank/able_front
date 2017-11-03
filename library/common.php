226f84eb756d2e801e84e4d8e423d05c5c14ecc5
chise44 <chise4455@gmail.com>
2017-11-03 13:10:47 +0900

    1

--- library/common.php
+++ library/common.php
@@ -2,15 +2,19 @@
 switch($_SERVER["HTTP_HOST"]){
     case "localhost" :
         $apiHost = "http://devapi.able-coin.io";
+        $dm = "http://localhost";
         break;
     case "dev.able-coin.io" :
         $apiHost = "http://devapi.able-coin.io";
+        $dm = "http://dev.able-coin.io";
         break;
     case "www.able-coin.io" :
         $apiHost = "https://api.able-coin.io";
+        $dm = "http://www.able-coin.io";
         break;
     default:
         $apiHost = "https://api.able-coin.io";
+        $dm = "http://www.able-coin.io";
         break;
 }
 ?>
