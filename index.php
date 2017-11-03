351e4f054ea2da08a971defb28e4c79d01bdd5cd
chise44 <chise4455@gmail.com>
2017-11-03 22:06:23 +0900

    ./

--- index.php
+++ index.php
@@ -930,7 +930,7 @@
   <script src="<?php echo $dm?>/js/jquery.waypoints.min.js"></script>
   <script src="<?php echo $dm?>/js/fadeInScroll.jQuery.js"></script>
   <script src="<?php echo $dm?>/js/modernizr.js"></script>
-  <script src="<?php echo $dm?>/js/main.js?d=201710201122"></script>
+  <script src="<?php echo $dm?>/js/main.js?d=201710201122322"></script>
   <script src="<?php echo $dm?>/js/scrollSpy.js" type="text/javascript"></script>
   <script src="<?php echo $dm?>/js/scrollto.js" type="text/javascript"></script>
   <script src="<?php echo $dm?>/js/anim_scroll.js" type="text/javascript"></script>
