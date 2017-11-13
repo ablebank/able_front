f66e9b7985cc3bb0eea7a83b897ad5ee9d95adaa
chise44 <chise4455@gmail.com>
2017-11-13 21:30:46 +0900

    1

--- index.php
+++ index.php
@@ -992,20 +992,20 @@
 <script type="text/javascript">
     function handleStart(evt) {
         evt.preventDefault();
-        log("touchstart.");
+        
         var el = document.getElementsByTagName("canvas")[0];
         var ctx = el.getContext("2d");
         var touches = evt.changedTouches;
 
         for (var i = 0; i < touches.length; i++) {
-            log("touchstart:" + i + "...");
+
             ongoingTouches.push(copyTouch(touches[i]));
             var color = colorForTouch(touches[i]);
             ctx.beginPath();
             ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
             ctx.fillStyle = color;
             ctx.fill();
-            log("touchstart:" + i + ".");
+
         }
     }
 
@@ -1020,27 +1020,27 @@
             var idx = ongoingTouchIndexById(touches[i].identifier);
 
             if (idx >= 0) {
-                log("continuing touch "+idx);
+
                 ctx.beginPath();
-                log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
+
                 ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-                log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
+
                 ctx.lineTo(touches[i].pageX, touches[i].pageY);
                 ctx.lineWidth = 4;
                 ctx.strokeStyle = color;
                 ctx.stroke();
 
                 ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-                log(".");
+
             } else {
-                log("can't figure out which touch to continue");
+
             }
         }
     }
 
     function handleEnd(evt) {
         evt.preventDefault();
-        log("touchend");
+
         var el = document.getElementsByTagName("canvas")[0];
         var ctx = el.getContext("2d");
         var touches = evt.changedTouches;
@@ -1058,14 +1058,14 @@
                 ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
                 ongoingTouches.splice(idx, 1);  // remove it; we're done
             } else {
-                log("can't figure out which touch to end");
+
             }
         }
     }
 
     function handleCancel(evt) {
         evt.preventDefault();
-        log("touchcancel.");
+
         var touches = evt.changedTouches;
 
         for (var i = 0; i < touches.length; i++) {
@@ -1080,7 +1080,6 @@
         el.addEventListener("touchend", handleEnd, false);
         el.addEventListener("touchcancel", handleCancel, false);
         el.addEventListener("touchmove", handleMove, false);
-        log("initialized.");
     }
 </script>
 </body>
