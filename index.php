0987153a2abdd50b9d89b873038123c180a68783
chise44 <chise4455@gmail.com>
2017-11-13 21:39:07 +0900

    1

--- index.php
+++ index.php
@@ -990,97 +990,10 @@
   <script src="<?php echo $dm?>/js/scrollto.js" type="text/javascript"></script>
   <script src="<?php echo $dm?>/js/anim_scroll.js" type="text/javascript"></script>
 <script type="text/javascript">
-    function handleStart(evt) {
-        evt.preventDefault();
-
-        var el = document.getElementsByTagName("canvas")[0];
-        var ctx = el.getContext("2d");
-        var touches = evt.changedTouches;
-
-        for (var i = 0; i < touches.length; i++) {
-
-            ongoingTouches.push(copyTouch(touches[i]));
-            var color = colorForTouch(touches[i]);
-            ctx.beginPath();
-            ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-            ctx.fillStyle = color;
-            ctx.fill();
-
-        }
-    }
-
-    function handleMove(evt) {
-        evt.preventDefault();
-        var el = document.getElementsByTagName("canvas")[0];
-        var ctx = el.getContext("2d");
-        var touches = evt.changedTouches;
-
-        for (var i = 0; i < touches.length; i++) {
-            var color = colorForTouch(touches[i]);
-            var idx = ongoingTouchIndexById(touches[i].identifier);
-
-            if (idx >= 0) {
-
-                ctx.beginPath();
-
-                ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-
-                ctx.lineTo(touches[i].pageX, touches[i].pageY);
-                ctx.lineWidth = 4;
-                ctx.strokeStyle = color;
-                ctx.stroke();
-
-                ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-
-            } else {
-
-            }
-        }
-    }
-
-    function handleEnd(evt) {
-        evt.preventDefault();
-
-        var el = document.getElementsByTagName("canvas")[0];
-        var ctx = el.getContext("2d");
-        var touches = evt.changedTouches;
-
-        for (var i = 0; i < touches.length; i++) {
-            var color = colorForTouch(touches[i]);
-            var idx = ongoingTouchIndexById(touches[i].identifier);
-
-            if (idx >= 0) {
-                ctx.lineWidth = 4;
-                ctx.fillStyle = color;
-                ctx.beginPath();
-                ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-                ctx.lineTo(touches[i].pageX, touches[i].pageY);
-                ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
-                ongoingTouches.splice(idx, 1);  // remove it; we're done
-            } else {
-
-            }
-        }
-    }
-
-    function handleCancel(evt) {
-        evt.preventDefault();
-
-        var touches = evt.changedTouches;
-
-        for (var i = 0; i < touches.length; i++) {
-            var idx = ongoingTouchIndexById(touches[i].identifier);
-            ongoingTouches.splice(idx, 1);  // remove it; we're done
-        }
-    }
-
-    function startup() {
-        var el = document.getElementsByTagName("canvas")[0];
-        el.addEventListener("touchstart", handleStart, false);
-        el.addEventListener("touchend", handleEnd, false);
-        el.addEventListener("touchcancel", handleCancel, false);
-        el.addEventListener("touchmove", handleMove, false);
-    }
+    $("canvas").css({
+        "overflow-y": "scroll",
+        "-webkit-overflow-scrolling": "touch"
+    });
 </script>
 </body>
 </html>
