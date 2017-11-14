54396944dec04d7bd7088a9e4b0581acbda99b52
chise44 <chise4455@gmail.com>
2017-11-14 14:15:12 +0900

    1

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
     <link rel="stylesheet" href="<?php echo $dm?>/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo $dm?>/css/flexslider.css" />
     <link rel="stylesheet" href="<?php echo $dm?>/css/animate.min.css" />
-    <link rel="stylesheet" href="<?php echo $dm?>/css/main.css?d=20170925131411" />
-    <link rel="stylesheet" href="<?php echo $dm?>/css/responsive.css?d=201709251323212" />
+    <link rel="stylesheet" href="<?php echo $dm?>/css/main.css?d=201709251314111" />
+    <link rel="stylesheet" href="<?php echo $dm?>/css/responsive.css?d=2017092513232122" />
     <link rel="shortcut icon" href="<?php echo $dm?>/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="<?php echo $dm?>/favicon.ico" type="image/x-icon" />
 
