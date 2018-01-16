f87dbb12fc29422818fde6bd66e84aa272e8c21b
chise44 <chise4455@gmail.com>
2018-01-16 17:25:37 +0900

    1

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
     <link rel="stylesheet" href="<?php echo $dm?>/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo $dm?>/css/flexslider.css" />
     <link rel="stylesheet" href="<?php echo $dm?>/css/animate.min.css" />
-    <link rel="stylesheet" href="<?php echo $dm?>/css/main.css?d=201709251315511" />
-    <link rel="stylesheet" href="<?php echo $dm?>/css/responsive.css?d=201709251412" />
+    <link rel="stylesheet" href="<?php echo $dm?>/css/main.css?d=2017092513155111" />
+    <link rel="stylesheet" href="<?php echo $dm?>/css/responsive.css?d=20170925141211" />
     <link rel="shortcut icon" href="<?php echo $dm?>/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="<?php echo $dm?>/favicon.ico" type="image/x-icon" />
 
